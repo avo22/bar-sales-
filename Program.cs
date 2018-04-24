9792cc8207e3f2dc06cb4f64dc007e13aa7e507e
mdwilliams006 <36497569+mdwilliams006@users.noreply.github.com>
2018-04-23 20:04:24 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bar_sales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
